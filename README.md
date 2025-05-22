6f1cea22d6d486e466b5795f9b0f161c78891d81
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2025-05-22 16:34:23 -0700

    Added infinite width and height

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
-# Organizacion del codigo
-## src
-Todo el codigo para que funcione la aplicacion
-## src/app
-Va a ir todas las screens de la aplicacion
-## src/components
-Todos los componentes que van a usar las screens
-## public
-Todos los elementos estaticos de la aplicacion, como imagenes
+# Representatation of relationships between users
+This project show the visual representation between people using nodes
--- src/app/graph/page.tsx
+++ src/app/graph/page.tsx
@@ -1,29 +1,78 @@
 'use client';
 
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import MasterControl from './MasterControl';
 import AddUser from './AddUser';
 
 function Graph() {
   const [isOpen, setOpen] = useState(false)
   const [isOpen2, setOpen2] = useState(false)
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+  const dragStart = useRef({x:0, y:0})
+  const isDragging = useRef (false)
+
+  function onMouseDown(e: React.MouseEvent<HTMLDivElement>){
+    isDragging.current = true
+    dragStart.current = {
+      x: e.clientX - offset.x,
+      y: e.clientY - offset.y
+    }
+  }
+
+  function onMouseUp(){
+    isDragging.current = false
+  }
+
+  function onMouseMove(e: React.MouseEvent<HTMLDivElement>) {
+    if (!isDragging.current) return
+    setOffset({
+      x: e.clientX - dragStart.current.x,
+      y: e.clientY - dragStart.current.y
+    })
+  }
+
+  function Center(){
+    setOffset({x:0,y:0})
+  }
 
   return (
     <>
-      <div>
-        <div className="flex justify-between gap-4 p-4">
-          <img src='/menu.svg' alt="Menu Icon" onClick={() => setOpen2(true)} className='size-12'/>
-          <img src='/add.svg' alt='Add' onClick={() => setOpen(true)}  className='size-12'/>
+      <div className='w-screen h-screen relative'>
+          {/* Header */}
+          <div className="flex justify-between gap-4 p-4"> 
+            <img src='/menu.svg' alt="Menu Icon" onClick={() => setOpen2(true)} className='size-12'/>
+            <button
+            className='bg-amber-500'
+            onClick={(Center)}
+            >
+            Center
+            </button>
+            <img src='/add.svg' alt='Add' onClick={() => setOpen(true)}  className='size-12'/>
+          </div>
+        {/* Here will be the graph using infinite scroll*/}
+        <div
+          className="w-screen h-screen overflow-hidden relative cursor-grab active:cursor-grabbing"
+          onMouseDown={onMouseDown}
+          onMouseMove={onMouseMove}
+          onMouseUp={onMouseUp}
+          onMouseLeave={onMouseUp}
+        >
+          <div 
+          className='absolute top-0 left-0' 
+          style={{transform: `translate(${offset.x}px, ${offset.y}px)`}}
+          >
+            <div className="flex flex-col items-center justify-center min-h-screen py-2 border-radius-lg bg">
+              <h1 className="text-4xl font-bold text-center text-black">
+                AAAAAAAAA
+              </h1>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col items-center justify-center min-h-screen py-2 border-radius-lg bg">
-          <h1 className="text-4xl font-bold text-center text-black">
-            AAAAAAAAA
-          </h1>
-        </div>
-      </div>
 
-      <AddUser isOpen={isOpen} onClose={() => setOpen(false)} />
-      <MasterControl isOpen={isOpen2} onClose={() => setOpen2(false)} />
+        {/* Here are the overlays */}
+        <AddUser isOpen={isOpen} onClose={() => setOpen(false)} />
+        <MasterControl isOpen={isOpen2} onClose={() => setOpen2(false)} />
+      </div>
     </>
   )
 }
